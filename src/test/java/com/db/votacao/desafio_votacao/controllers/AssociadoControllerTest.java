45224c564245196a065d00ee3202aeeb2d988001
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-09-01 10:24:21 -0300

    Subdtituindo a anotação @MockBean(deprecada), pela anotação @MockitoBean nos testes unitarios da controller de associados.

--- src/test/java/com/db/votacao/desafio_votacao/controllers/AssociadoControllerTest.java
+++ src/test/java/com/db/votacao/desafio_votacao/controllers/AssociadoControllerTest.java
@@ -16,12 +16,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -41,7 +41,7 @@ class AssociadoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AssociadoService service;
 
     @Autowired
